90f1b7dc66f8617b814eee67c3a3a4ad97f4624a
sunny patel <sunnykpatel.skp@gmail.com>
2023-10-16 17:09:28 -0400

    add create books html

--- src/index.js
+++ src/index.js
@@ -1,20 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-function Greeting() {
+const BookList = () => {
   return (
-    <div>
-      <Person />
-      <Msg />
-    </div>
+    <section>
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
   );
-}
+};
 
-const Person = () => <h2>john doe</h2>;
-const Msg = () => {
-  return <h2>msg</h2>;
+const Book = () => {
+  return (
+    <article>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+const Image = () => (
+  <img
+    src="https://images-na.ssl-images-amazon.com/images/I/91ke43dIxkL._AC_UL600_SR600,400_.jpg"
+    alt="Iron Flame (The Empyrean, 2)"
+  />
+);
+const Title = () => <h2>Iron Flame (The Empyrean, 2)</h2>;
+const Author = () => {
+  return <h4>Rebecca Yarros</h4>;
 };
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<Greeting />);
+root.render(<BookList />);
