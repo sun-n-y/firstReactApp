66f115c84ecbb2b650950ade8f15cd8e27c65cd3
sunny patel <sunnykpatel.skp@gmail.com>
2023-10-17 11:32:05 -0400

    add props destructuring

--- src/index.js
+++ src/index.js
@@ -32,13 +32,12 @@ const BookList = () => {
 };
 
 // book component
-const Book = (props) => {
-  console.log(props);
+const Book = ({ img, title, author }) => {
   return (
     <article className="book">
-      <img src={props.img} alt={props.title} />
-      <h2>{props.title}</h2>
-      <h4>{props.author}</h4>
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
     </article>
   );
 };
