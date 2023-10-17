916526ddf1281d0c3d7ef6eef3794c66088b0ac4
sunny patel <sunnykpatel.skp@gmail.com>
2023-10-17 15:20:37 -0400

    add local images in src

--- src/books.js
+++ src/books.js
@@ -1,14 +1,24 @@
+import img1 from './images/book1.jpg';
+import img2 from './images/book2.jpg';
+import img3 from './images/book3.jpg';
+
 export const books = [
   {
     id: 1,
     title: 'Outlive',
     author: 'Petter Attia',
-    img: './images/book.jpg',
+    img: img1,
   },
   {
     id: 2,
     title: 'The Shadow Work Journal',
     author: 'Keila Shaheen',
-    img: 'https://images-na.ssl-images-amazon.com/images/I/51cB5c5e1DL._AC_UL600_SR600,400_.jpg',
+    img: img2,
+  },
+  {
+    id: 3,
+    title: 'Fourth Wing',
+    author: 'Rebecca Yarros',
+    img: img3,
   },
 ];
