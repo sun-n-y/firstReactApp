0ba8ddb8811c84c510ff18be880a77880fdd2b62
sunny patel <sunnykpatel.skp@gmail.com>
2023-10-17 15:31:57 -0400

    add book number

--- src/Book.js
+++ src/Book.js
@@ -1,12 +1,13 @@
 // book component
 const Book = (props) => {
-  const { img, title, author } = props;
+  const { img, title, author, index } = props;
   // console.log(props);
   return (
     <article className="book">
       <img src={img} alt={title} />
       <h2>{title}</h2>
       <h4>{author}</h4>
+      <span className="number">#{index + 1}</span>
     </article>
   );
 };
