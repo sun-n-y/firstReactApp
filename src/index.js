d487df9a7b5229bb93db693ca8451523961142e4
sunny patel <sunnykpatel.skp@gmail.com>
2023-10-17 11:59:11 -0400

    add simple list

--- src/index.js
+++ src/index.js
@@ -3,32 +3,27 @@ import ReactDOM from 'react-dom/client';
 
 import './index.css';
 
-const firstBook = {
-  title: 'Outlive',
-  author: 'Petter Attia',
-  img: './images/book.jpg',
-};
-const secondBook = {
-  title: 'The Shadow Work Journal',
-  author: 'Keila Shaheen',
-  img: 'https://images-na.ssl-images-amazon.com/images/I/51cB5c5e1DL._AC_UL600_SR600,400_.jpg',
-};
+const books = [
+  {
+    title: 'Outlive',
+    author: 'Petter Attia',
+    img: './images/book.jpg',
+  },
+  {
+    title: 'The Shadow Work Journal',
+    author: 'Keila Shaheen',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/51cB5c5e1DL._AC_UL600_SR600,400_.jpg',
+  },
+];
+
+const names = ['s', 'k', 'p'];
+const newNames = names.map((name) => {
+  console.log(name);
+  return <h1>{name}</h1>;
+});
 
 const BookList = () => {
-  return (
-    <section className="booklist">
-      <Book
-        author={firstBook.author}
-        title={firstBook.title}
-        img={firstBook.img}
-      />
-      <Book
-        author={secondBook.author}
-        title={secondBook.title}
-        img={secondBook.img}
-      />
-    </section>
-  );
+  return <section className="booklist">{newNames}</section>;
 };
 
 // book component
